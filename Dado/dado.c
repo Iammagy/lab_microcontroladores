af103389d57cc971b1fb88e8873e2eb4b0498198
Magaly Garcia <maggy@Magalys-MacBook-Air.local>
2025-03-23 02:50:20 -0600

    Simulacion y definicion de funciones

---
+++ Dado/dado.c
@@ -0,0 +1,31 @@
+#include <pic14/pic12f675.h>
+
+//#pragma config WDTE = OFF //watchdog off
+//#pragma config MCLRE = ON // MCLR Pin Function Select bit (MCLR pin function is MCLR, MCLR externally set)
+
+int random_number(void);
+
+void main(void)
+{
+
+    int number = random_number();
+
+    TRISIO = 0b00000000; //pins as outputs except IO3
+	GPIO = 0x00; //leds off initially (0V)
+    
+    TRISIO1	= 1; /* Use GP1 as input */
+	ANSEL = 0;
+	ANS0  = 1; /* Use AN3(GP0) as analog input */
+	CHS1  = 1, CHS0 = 1; /* Use ch.3 (AN3) */
+    
+    int delay_time;
+
+    while (1)
+    {
+
+    }
+}
+
+int random_number(void){
+    return 6;
+}
