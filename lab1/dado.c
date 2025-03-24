b6886fef16a258e10d5adfe0b96689a8d86b9264
Iammagy <lidia.garcia@ucr.ac.cr>
2025-03-23 21:53:09 -0600

    Cambios en esquematico y switch del codigo

--- lab1/dado.c
+++ lab1/dado.c
@@ -1,5 +1,8 @@
 #include <pic14/pic12f675.h>
 
+typedef unsigned int word ;
+word __at 0x2007 __CONFIG = (_WDTE_OFF);
+
 int random_number(void);
 
 void main(void)
@@ -7,22 +10,48 @@ void main(void)
 
     int number = random_number();
 
-    TRISIO = 0b00000000; //pins as outputs except IO3
+    TRISIO = 0b00000000; //pins as outputs except IO3 
 	GPIO = 0x00; //leds off initially (0V)
     
-    TRISIO1	= 1; /* Use GP1 as input */
-	ANSEL = 0;
-	ANS0  = 1; /* Use AN3(GP0) as analog input */
-	CHS1  = 1, CHS0 = 1; /* Use ch.3 (AN3) */
+	//ANSEL = 0b00000001;// Usa el gp0 como analogico
     
-    int delay_time;
+    //int delay_time;
 
     while (1)
     {
+        switch (number)
+        {
 
+        case 1:
+        GPIO = 0b00000001;
+        break;
+
+        case 2:
+        GPIO = 0b00000010;
+        break;
+
+        case 3:
+        GPIO = 0b00000011;
+        break;
+
+        case 4:
+        GPIO = 0b00100010;
+        break;
+
+        case 5:
+        GPIO = 0b00100011;
+        break;
+
+        case 6:
+        GPIO = 0b00100110;
+        break;
+
+        default: 
+            GPIO = 0x00;
+        }   
     }
 }
 
 int random_number(void){
-    return 6;
+    return 5;
 }
